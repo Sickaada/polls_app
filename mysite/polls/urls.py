adf97384e8e78f36d7e1beecec948e6088e8e202
silentC1C4D4 <silentcicada3301@gmail.com>
2020-05-11 16:19:36 +0530

    creating frontend

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
+# <xyz> <--- this captures the int part of the url
 urlpatterns = [
-    path('', views.questions)
+    path('', views.index, name='index'),
+    path('<int:question_id>/',views.details, name = 'details'),
+    path('<int:question_id>/results/',views.results, name = 'results'),
+    path('<int:question_id>/vote/',views.vote, name = 'vote')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,32 @@
 # Sort of front end is written here
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
 
-def questions(a):
-    return HttpResponse("Hello World!")
+def details(request, question_id):
+    return HttpResponse("You're looking at the question %s." %question_id)
+
+def results(request,question_id):
+    return HttpResponse("You're looking at the result of question %s." %question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on the question %s." %question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-published_date')[:5]
+    
+    # following key will be rendered as value
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return render(request,'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("The Question doesn't exist.")
+        return render(request, 'polls/details.html', {"question": question})
 
